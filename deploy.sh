fbaa6a7177871c7c5055c7b5f84b05a4d72d4f1b
Vennilavan <77039703+Vennilavan12@users.noreply.github.com>
2023-11-10 10:14:54 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-docker-compose up -d
+# Check the Git branch
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    ./build.sh
+
+    # Log in to Docker Hub (replace with your actual Docker Hub credentials)
+    docker login -u vennilavan -p dckr_pat_VPY-9NwBDEwI0PpfGcF3l8ujH28
+
+  
+    # Tag the image
+    docker tag react-appimg sayeda77/dev
+
+    # Push the image to the Dev Docker Hub repository
+    docker push vennilavan/dev
+
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
+    # Build your project
+    ./build.sh
+
+    # Log in to Docker Hub (replace with your actual Docker Hub credentials)
+    docker login -u sayeda77 -p dckr_pat_VPY-9NwBDEwI0PpfGcF3l8ujH28
+
+   
+    # Tag the image
+    docker tag react-appimg vennilavan/prod 
+
+    # Push the image to the Prod Docker Hub repository
+    docker push vennilavan/prod
+else
+    echo "Deployment error"
+fi
